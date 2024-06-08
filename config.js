c2f3d8cee4e852701a86360678105248eecac42d
alexbhai001 <hmmmhoxd@gmail.com>
2024-06-09 01:19:01 +0500

    Update config.js

--- config.js
+++ config.js
@@ -19,20 +19,20 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "wasixml@gmail.com";
-global.location = "Lahore,Pakistan.";
+global.email = "umarofficial404@gmail.com";
+global.location = "Rawalpindi,Pakistan.";
 global.mongodb = process.env.MONGODB_URI || "";
-global.allowJids = process.env.ALLOW_JID || "923192173398@s.whatsapp.net";
+global.allowJids = process.env.ALLOW_JID || "918891693142@s.whatsapp.net";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
-global.github = process.env.GITHUB || "https://github.com/Itxxwasi/WASI-MD-V2";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j";
-global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/1f4e6e1cc2ed1662adb36.jpg";
-global.devs = "923135673658";
-global.sudo = process.env.SUDO || "923135673658,923192173398";
-global.owner = process.env.OWNER_NUMBER || "923135673658";
+global.github = process.env.GITHUB || "https://github.com/Um4r719/UD-Ai";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vac3lOz7Noa9iXEHe50m";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vac3lOz7Noa9iXEHe50m";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/bab19f9f1ccf2bafbecff.jpg";
+global.devs = "918891693142";
+global.sudo = process.env.SUDO || "918891693142,918891693142";
+global.owner = process.env.OWNER_NUMBER || "918891693142";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -51,7 +51,7 @@ global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
 
 global.api_smd = "https://api-smd-1.vercel.app";
-global.scan = "https://wasimdscanner-68feefafb737.herokuapp.com/";
+global.scan = "https://ud-ai-session.onrender.com/pair/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
@@ -61,12 +61,12 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "𝐅𝐈𝐋𝐄-𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃-𝐁𝐘-𝐖𝐀𝐒𝐈-𝐌𝐃-𝐕2😍",
-  author: process.env.PACK_AUTHER || "WASO",
-  packname: process.env.PACK_NAME || "♥️",
-  botname: process.env.BOT_NAME || "𝐖𝐀𝐒𝐈-𝐌𝐃-𝐕2",
-  ownername: process.env.OWNER_NAME || "WASI",
-  errorChat: process.env.ERROR_CHAT || "923192173398",
+  caption: process.env.CAPTION || "Bʏ Uᴅ Aɪ",
+  author: process.env.PACK_AUTHER || "Uᴍᴀʀ",
+  packname: process.env.PACK_NAME || "Uᴅ Aɪ",
+  botname: process.env.BOT_NAME || "Uᴅ Aɪ",
+  ownername: process.env.OWNER_NAME || "Uᴍᴀʀ",
+  errorChat: process.env.ERROR_CHAT || "918891693142",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
